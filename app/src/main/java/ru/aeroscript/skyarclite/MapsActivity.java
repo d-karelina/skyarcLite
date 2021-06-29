b7ec92b511233f693e0b9d0a6d8648ab243d2311
dana <daneliya.karelina@gmail.com>
2021-06-29 23:11:19 +0300

    some updates

--- app/src/main/java/ru/aeroscript/skyarclite/MapsActivity.java
+++ app/src/main/java/ru/aeroscript/skyarclite/MapsActivity.java
@@ -18,6 +18,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Objects;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -29,7 +30,7 @@ import ru.aeroscript.skyarclite.zones.Zone;
 
 import static ru.aeroscript.skyarclite.Settings.*;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnCameraMoveListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnCameraIdleListener {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
@@ -49,6 +50,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
 
+
+
     }
 
 
@@ -60,9 +63,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng spb = new LatLng(59.563178, 30.188478);
         //mMap.addMarker(new MarkerOptions().position(spb).title("Marker in SPb").icon(
         //        BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+
         mMap.moveCamera(CameraUpdateFactory.newLatLng(spb));
 
-        mMap.setOnCameraMoveListener(this::onCameraMove);
+        mMap.setOnCameraIdleListener(this) ;
+        mMap.setMinZoomPreference(6.0f);
+        mMap.setMaxZoomPreference(14.0f);
         //getZones() ;
     }
 
@@ -79,17 +85,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Callback callback = new Callback() {
             @Override
             public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                Log.i("провал","случился") ;
 
             }
 
             @Override
             public void onResponse(@NotNull Call call, @NotNull Response response)  {
-                String geo = String.valueOf(response.body());
-                ArrayList<Zone> zones = JsonParsers.parseManagedZones(geo) ;
-                for (Zone zone: zones) {
-                    new GeometryBuilder().buildPolygon(zone.getCoordinates()) ;
+                String geo = null;
+                try {
+                    geo = Objects.requireNonNull(response.body()).string();
+                    Log.i("json",geo) ;
+                    ArrayList<Zone> zones = JsonParsers.parseManagedZones(geo) ;
+                    for (Zone zone: zones) {
+                        new GeometryBuilder().buildPolygon(zone.getCoordinates()) ;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-
             }
         };
         try {
@@ -102,7 +114,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     @Override
-    public void onCameraMove() {
+    public void onCameraIdle() {
         getZones() ;
     }
 
@@ -111,11 +123,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             PolygonOptions polygonOptions = new PolygonOptions()
                     .fillColor(Color.argb(70, 0, 255, 255)).strokeWidth(1)
                     .strokeColor(Color.DKGRAY) ;
+            /*for (int i = 0; i < coordinates.size(); i++) {
+                polygonOptions.add(coordinates.get(i)) ;
+            }*/
             for (LatLng coordinate: coordinates) {
                 polygonOptions.add(coordinate) ;
             }
 
-            mMap.addPolygon(polygonOptions) ;
+            runOnUiThread(() -> mMap.addPolygon(polygonOptions)) ;
+
             Log.i("рисование","должно быть выполнено") ;
         }
 
